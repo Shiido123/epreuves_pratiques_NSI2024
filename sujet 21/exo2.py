2b9816707437b131b7bb322e304e7ac175f6324a
Shiido <shiido35@gmail.com>
2024-03-17 21:58:28 +0100

    exo 2 sujet 21

--- sujet 21/exo2.py
+++ sujet 21/exo2.py
@@ -3,17 +3,19 @@ def parcours(adj, x, acc):
     du graphe donné par les listes d'adjacence adj 
     depuis le sommet x en accumulant les sommets
     rencontrés dans acc'''
-    if x ...: 
+    if x not in acc:
         acc.append(x)
-        for y in ...: 
-            parcours(adj, ...) 
+        for y in adj[x]:
+            parcours(adj, y, acc)
+
 
 def accessibles(adj, x):
     '''Renvoie la liste des sommets accessibles dans le
     graphe donné par les listes d'adjacence adj depuis
     le sommet x.'''
     acc = []
-    parcours(adj, ...) 
+    parcours(adj, x, acc)
     return acc
 
-
+print(accessibles([[1, 2], [0], [0, 3], [1], [5], [4]], 0))
+print(accessibles([[1, 2], [0], [0, 3], [1], [5], [4]], 4))
