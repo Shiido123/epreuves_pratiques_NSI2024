b4c08f72bff2b78815b398e7f80c5fc50e05711a
Shiido <shiido35@gmail.com>
2024-03-19 10:30:25 +0100

    exo 1 et 2 sujet 47

---
+++ sujet 47/exo1.py
@@ -0,0 +1,11 @@
+def max_dico(dico):
+    maxi = ("Antoine le bot", 0)
+    for cle in dico.keys():
+        if dico[cle] > maxi[1]:
+            maxi = (cle, dico[cle])
+
+    return maxi
+
+
+print(max_dico({'Bob': 102, 'Ada': 201, 'Alice': 103, 'Tim': 50}))
+print(max_dico({'Alan': 222, 'Ada': 201, 'Eve': 222, 'Tim': 50}))
