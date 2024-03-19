65e9509c5c79a437f9cd92c772559acc655285a4
Shiido <shiido35@gmail.com>
2024-03-19 09:33:04 +0100

    exo 2 sujet 40

--- sujet 40/exo2.py
+++ sujet 40/exo2.py
@@ -4,15 +4,19 @@ def trouver_intrus(tab, g, d):
     tab vérifie les conditions de l'exercice,
     g et d sont des multiples de 3."""
     if g == d:
-        return tab
+        return tab[g]
     else:
         nombre_de_triplets = (d - g) // 3
         indice = g + 3 * (nombre_de_triplets // 2)
-        if ...:
-            return ...
+        if tab[indice] != tab[indice+1]:
+            return trouver_intrus(tab, g, indice)
         else:
-            return ...
+            return trouver_intrus(tab, indice+3, d)
 
 
 print(trouver_intrus([3, 3, 3, 9, 9, 9, 1, 1, 1, 7,
                       2, 2, 2, 4, 4, 4, 8, 8, 8], 0, 18))
+print(trouver_intrus([8, 5, 5, 5, 9, 9, 9, 18, 18, 18, 3, 3, 3],
+                     0, 12))
+print(trouver_intrus([5, 5, 5, 1, 1, 1, 0, 0, 0,
+                      6, 6, 6, 3, 8, 8, 8], 0, 15))
