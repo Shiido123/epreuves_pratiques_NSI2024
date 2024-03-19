1cb05b40f4cfb218902b27f44bb607d9446a8994
Shiido <shiido35@gmail.com>
2024-03-19 22:12:26 +0100

    exo 2 sujet 13

--- sujet 13/exo2.py
+++ sujet 13/exo2.py
@@ -7,10 +7,14 @@ def insere(tab, a):
     tab_a = [ a ] + tab # nouveau tableau contenant a 
                         # suivi des éléments de tab
     i = 0
-    while i < ... and a > ...: 
-        tab_a[i] = ... 
+    while i < len(tab) and a > tab[i]: 
+        tab_a[i] = tab[i] 
         tab_a[i+1] = a
-        i = ... 
+        i = i + 1 
     return tab_a
 
+print(insere([1, 2, 4, 5], 3))
+print(insere([1, 2, 7, 12, 14, 25], 30))
+print(insere([2, 3, 4], 1))
+print(insere([], 1))
 
