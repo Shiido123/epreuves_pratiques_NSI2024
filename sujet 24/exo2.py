dd20d4c18a4b43052d7524b23e112aea9ccf1339
Shiido <shiido35@gmail.com>
2024-03-20 19:34:51 +0100

    exo 2 sujet 24

--- sujet 24/exo2.py
+++ sujet 24/exo2.py
@@ -3,21 +3,21 @@ def somme_max(tab):
     sommes_max = [0]*n
     sommes_max[0] = tab[0]
     # on calcule la plus grande somme se terminant en i
-    for i in range(1,n):
-        if sommes_max[i-1] + tab[i] > tab[i]: 
+    for i in range(1, n):
+        if sommes_max[i-1] + tab[i] > tab[i]:
             sommes_max[i] = sommes_max[i-1] + tab[i]
         else:
             sommes_max[i] = tab[i]
     # on en déduit la plus grande somme de celles-ci
     maximum = 0
     for i in range(1, n):
-        if tab[i] > maximum: 
+        if sommes_max[i] > sommes_max[maximum]:
             maximum = i
 
-    return sommes_max[maximum] 
+    return sommes_max[maximum]
+
 
 print(somme_max([1, 2, 3, 4, 5]))
 print(somme_max([1, 2, -3, 4, 5]))
 print(somme_max([1, 2, -2, 4, 5]))
-print(somme_max([1, -2, 3, 10, -4, 7, 2, -5])) #marche pas encore
-
+print(somme_max([1, -2, 3, 10, -4, 7, 2, -5]))  # marche pas encore
