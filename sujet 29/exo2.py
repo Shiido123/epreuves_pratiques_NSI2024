05d343c81225455e8243057e65863e213870b2cc
Shiido <shiido35@gmail.com>
2024-03-20 19:24:06 +0100

    pascal exo 2 sujet 29

--- sujet 29/exo2.py
+++ sujet 29/exo2.py
@@ -1,17 +1,21 @@
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...] 
-    for i in range(...): 
-        ligne_suiv.append(...) 
-    ligne_suiv.append(...) 
+    ligne_suiv = [1]
+    for i in range(len(ligne) - 1):
+        ligne_suiv.append(ligne[i] + ligne[i + 1])
+    ligne_suiv.append(1)
     return ligne_suiv
+
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
-    triangle = [ [1] ]
-    for k in range(...): 
-        ligne_k = ... 
+    triangle = [[1]]
+    for k in range(n - 1):
+        ligne_k = ligne_suivante(triangle[k-1])
         triangle.append(ligne_k)
     return triangle
 
 
+print(ligne_suivante([1, 3, 3, 1]))
+print(pascal(2))
+print(pascal(3))
