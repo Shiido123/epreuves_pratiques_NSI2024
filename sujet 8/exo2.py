4760f7a9a12cf54e6afd09610ff9755e36fcc5fb
Shiido <shiido35@gmail.com>
2024-03-20 19:11:55 +0100

    exo 2 sujet 8

--- sujet 8/exo2.py
+++ sujet 8/exo2.py
@@ -18,12 +18,26 @@ class Expr:
     def infixe(self):
         """renvoie la représentation infixe de l'expression en
         chaine de caractères"""
-        s = ... 
+        s = ""
         if self.gauche is not None:
-            s = '(' + s + ... .infixe() 
-        s = s + ... 
-        if ... is not None: 
-            s = s + ... + ... 
+            s = '(' + s + self.gauche.infixe()
+        s = s + str(self.valeur)
+        if self.droite is not None:
+            s = s + self.droite.infixe() + ')'
         return s
 
 
+a = Expr(Expr(None, 1, None), '+', Expr(None, 2, None))
+print(a.infixe())
+
+b = Expr(Expr(Expr(None, 1, None), '+', Expr(None, 2, None)),
+         '*', Expr(Expr(None, 3, None), '+', Expr(None, 4, None)))
+
+print(b.infixe())
+
+e = Expr(
+    Expr(Expr(None, 3, None), '*', Expr(Expr(None, 8, None),
+                                        '+', Expr(None, 7, None))),
+    '-', Expr(Expr(None, 2, None), '+', Expr(None, 1, None)))
+
+print(e.infixe())
