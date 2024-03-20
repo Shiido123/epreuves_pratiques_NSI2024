eab914f24119d41cdbaaab53306113f959481126
Shiido <shiido35@gmail.com>
2024-03-20 19:28:42 +0100

    exo 2 sujet 27

--- sujet 27/exo2.py
+++ sujet 27/exo2.py
@@ -4,13 +4,15 @@ def colore_comp1(M, i, j, val):
 
     M[i][j] = val
 
-    if i-1 >= 0: # propage à gauche
+    if i-1 >= 0:  # propage à gauche
         colore_comp1(M, i-1, j, val)
-    if ... < len(M): # propage à droite 
-        colore_comp1(M, ..., j, val) 
-    if ...: # propage en haut 
-        colore_comp1(M, ..., ..., val) 
-    if ...: # propage en bas 
-        ...
+    if i+1 < len(M):  # propage à droite
+        colore_comp1(M, i+1, j, val)
+    if j+1:  # propage en haut
+        colore_comp1(M, i, j+1, val)
+    if j-1:  # propage en bas
+        colore_comp1(M, i, j-1, val)
 
-
+M = [[0, 0, 1, 0], [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0]]
+colore_comp1(M, 2, 1, 3)
+print(M)
